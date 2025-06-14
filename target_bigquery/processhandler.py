fbe8b395934617ca7ddbf28c11706ca265174a7c
DmitreyZl <70319100+DmitreyZl@users.noreply.github.com>
2025-06-14 11:18:18 +0300

    Update processhandler.py

--- target_bigquery/processhandler.py
+++ target_bigquery/processhandler.py
@@ -365,7 +365,7 @@ class LoadJobProcessHandler(BaseProcessHandler):
         # schema = build_schema(schema_simplified, key_properties=key_props, add_metadata=metadata_columns,
         #                       force_fields=force_fields)
         load_config = LoadJobConfig()
-        load_config.ignore_unknown_values = True
+        load_config.ignore_unknown_values = False
         load_config.schema = table_schema
 
         # partitioning
